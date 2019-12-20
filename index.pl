f56dfd22d9ce1f04d489863360b7f9ae6c78ad67
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-12-20 11:55:10 +0000

    new method of processing

--- index.pl
+++ index.pl
@@ -16,13 +16,24 @@ _HEADER
 # process the big html document
 my $content = 1;
 my $contentFile = "";
+my @sections = [];
+
+      # print "open content file to write here/n"
+      # print "open index file to write here/n";
+      # print "close content file here/n";
+
+# process in 2 passes:
+# 1. read section headings into an array
+# - open index file & process heading as below
+# - copy file names into the array
+# 2. copy sections into a new file
+# - at each heading open a new file
+# - copy contents until the next heading
+# - close the previous section's file & copy new contents
+#
+
 while (<DATA>){
   if ($_ =~ "H1"){
-    if ($content == 1) {
-      #print "open content file to write here/n"
-      print "open index file to write here/n"
-      print "close content file here/n"
-
      my $index_file = substr($_, 7,-6);
      $index_file =~  s/(?<!^)\s(?!$)/_/g;
      
@@ -31,19 +42,10 @@ while (<DATA>){
      $line = "$line</a>";
      # s/'/H1'/'/a'/;
      print "$line \n";
-
-     $content = 0;
-     $contentFile = $index_file;
+     @filenames.append($index_file);
     }
-  } else {
-    if ($content == 0){
-      print "open content file to write here/n"
-
-    }
-
-
-  }
 }
+print @filenames;
 
 print <<_FOOTER;
   </body>
